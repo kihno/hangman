04028660de83022507dd7e914b6ba73bb8395ab1
kihno <stistyle13@gmail.com>
2023-02-16 16:58:17 -0800

    Create game start function

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,57 +1,81 @@
-def get_random_word
-  all_words = []
-  File.readlines('google-10000-english.txt').each { |word| all_words << word.chomp }
+class Game  
+  HANGMANPICS = ['''
+    +---+
+    |   |
+    |   
+    |
+    |   
+    |   
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |   
+    |   
+    |   
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |   |
+    |   
+    |   
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |  /|
+    |   
+    |   
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |  /|\
+    |   
+    |   
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |  /|\
+    |  /
+    |    
+  =========''', '''
+    +---+
+    |   |
+    |   O
+    |  /|\
+    |  / \
+    |    
+  =========''']
 
-  all_words.select { |word| word.length > 5 && word.length < 12 }.sample
+  def get_random_word
+    all_words = []
+    File.readlines('google-10000-english.txt').each { |word| all_words << word.chomp }
+  
+    all_words.select { |word| word.length > 5 && word.length < 12 }.sample
+  end
+
+  def game_start
+    @secret_word = get_random_word
+    @turn = 0
+    @guess = Array.new(@secret_word.length, '_')
+
+    puts "Welcome to Hangman"
+    puts HANGMANPICS[@turn]
+    puts "  #{@guess.join(' ')}"
+    puts "\n"
+  end
+
+  def game_loop
+    
+  end
+
+  def save_game
+
+  end
 end
 
-HANGMANPICS = ['''
-  +---+
-  |   |
-  |   
-  |
-  |   
-  |   
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |   
-  |   
-  |   
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |   |
-  |   
-  |   
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |  /|
-  |   
-  |   
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |  /|\
-  |   
-  |   
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |  /|\
-  |  /
-  |    
-=========''', '''
-  +---+
-  |   |
-  |   O
-  |  /|\
-  |  / \
-  |    
-=========''']
+new_game = Game.new
+new_game.game_start
